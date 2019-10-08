4c9434ecaa8158aad3954a4fed7ee7fda8ca6278
saula <saulalonsopalazuelos@outlook.com>
2019-10-07 20:35:06 -0600

    0.01d added further dataline validation

--- src/com/Enlaces/Main.java
+++ src/com/Enlaces/Main.java
@@ -2,7 +2,6 @@ package com.Enlaces;
 
 import Util.Consola;
 
-import javax.xml.stream.util.EventReaderDelegate;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -22,7 +21,7 @@ public class Main {
         FileReader fileReader = new FileReader("src/Data/input");
         BufferedReader in = new BufferedReader(fileReader);
 
-        Pattern pattern = Pattern.compile("(([A-z]+[0-9]*)[\\s]*(<-|->|<=|=>|-)[\\s]*([A-z]+[0-9]*)[\\s]*[.?])(.*)");
+        Pattern pattern = Pattern.compile("((^[A-z][A-z0-9]{0,14})[\\s]*(<-|->|<=|=>|-)[\\s]*([A-z][A-z0-9]{0,14})[\\s]*([.?]))(.*)");
 
         String data;
         while ((data = in.readLine()) != null) {
@@ -33,12 +32,18 @@ public class Main {
                 String city1 = m.group(2);
                 String operator = m.group(3);
                 String city2 = m.group(4);
-                String garbage = m.group(5);
+                String garbage = m.group(6);
+                String dotMark = m.group(5);
+
+                boolean doktMarkCheck = ((operator.equals("<=") || operator.equals("=>")) && dotMark.equals(".")) || ((operator.equals("<-") || operator.equals("->")) && dotMark.equals("?"));
+                System.out.println(operator + " && " + dotMark + " : " + doktMarkCheck);
+                if(doktMarkCheck) continue;
 
                 System.out.println();
                 System.out.println(Consola.Color.RED + "Match in line " + lineCounter + " : " + Consola.Color.RESET + line + Consola.Color.RESET);
                 System.out.println(Consola.Color.BLUE + "Expression: " + Consola.Color.RESET + expresion);
-                if(!garbage.isBlank()) System.out.println(Consola.Color.CYAN + "Garbage: " + Consola.Color.RESET + garbage);
+                if (!garbage.isBlank())
+                    System.out.println(Consola.Color.CYAN + "Garbage: " + Consola.Color.RESET + garbage);
 
                 System.out.print(Consola.Color.PURPLE + "City1 : " + Consola.Color.RESET + city1 + " : ");
                 System.out.print(Consola.Color.GREEN);
@@ -63,12 +68,12 @@ public class Main {
                         map.createRelation(city2, city1);
                         break;
                     case "=>":
-                        System.out.println("Puede ir de " + city1 + " a " +  city2 + " : " + map.searchWayToCityBFS(city1, city2));
-                        System.out.println("Puede ir de " + city1 + " a " +  city2 + " : " + map.searchWayToCityDFS(city1, city2));
+                        System.out.println("Puede ir de " + city1 + " a " + city2 + " : " + map.searchWayToCityBFS(city1, city2));
+                        System.out.println("Puede ir de " + city1 + " a " + city2 + " : " + map.searchWayToCityDFS(city1, city2));
                         break;
                     case "<=":
-                        System.out.println("Puede ir de " + city2 + " a " +  city1 + " : " + map.searchWayToCityBFS(city2, city1));
-                        System.out.println("Puede ir de " + city2 + " a " +  city1 + " : " + map.searchWayToCityDFS(city2, city1));
+                        System.out.println("Puede ir de " + city2 + " a " + city1 + " : " + map.searchWayToCityBFS(city2, city1));
+                        System.out.println("Puede ir de " + city2 + " a " + city1 + " : " + map.searchWayToCityDFS(city2, city1));
                         break;
                     case "-":
                         map.closeAllConnectionsBetween(city1, city2);
@@ -77,5 +82,13 @@ public class Main {
             }
             lineCounter++;
         }
+    }
+
+    public interface GraphSearch {
+        int nodeSearch(Map.City a, Map.City b);
+    }
+
+    private int operate(Map.City a, Map.City b, GraphSearch search) {
+        return search.nodeSearch(a, b);
     }
 }
